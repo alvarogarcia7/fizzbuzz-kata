9348f59af6339e6e1994b4e75be37301c8a02bd4
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:25:59 +0200

    refactor: extract method for checking divisibility by 3

--- src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
@@ -3,18 +3,22 @@ package com.example.kata.fizzbuzz;
 public class FizzBuzz {
     String getFizzBuzzFor(int n) {
         String result = null;
-        if (n % 3 == 0 && n >= 6) {
+        if (isDivisibleBy(n, 3) && n >= 6) {
             result = "Fizz";
         }
         if (n % 5 == 0) {
             result = "Buzz";
         }
-        if (n % 5 == 0 && n % 3 == 0 && n >= 30) {
+        if (n % 5 == 0 && isDivisibleBy(n, 3) && n >= 30) {
             result = "FizzBuzz";
         }
         if (result == null) {
             result = String.valueOf(n);
         }
         return result;
+    }
+
+    private boolean isDivisibleBy(int n, int divisor) {
+        return n % divisor == 0;
     }
 }
