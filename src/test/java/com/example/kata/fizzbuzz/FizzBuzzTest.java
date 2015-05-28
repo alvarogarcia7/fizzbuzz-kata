cfad70c2ab9acf0aec7ba513659e96e013e57065
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:12:16 +0200

    red: all non-special cases should return the same number

---
+++ src/test/java/com/example/kata/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package com.example.kata.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzTest {
+    @Test
+    public void should_return_the_same_value_for_non_multiples_of_3_or_5() {
+        assertThat(fizzbuzz(3), is("3"));
+    }
+
+    private String fizzbuzz(int n) {
+        return null;
+    }
+
+
+}
