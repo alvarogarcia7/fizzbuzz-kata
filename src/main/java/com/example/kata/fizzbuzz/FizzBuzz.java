59d93e72e877d50a7d962ab5e6c683c7fae4814c
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:28:18 +0200

    refactor: for symmetry, prefer having a higher-level function to indicate divisibility by 3

--- src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
@@ -3,19 +3,23 @@ package com.example.kata.fizzbuzz;
 public class FizzBuzz {
     String getFizzBuzzFor(int n) {
         String result = null;
-        if (isDivisibleBy(n, 3) && n >= 6) {
+        if (isDivisibleBy3(n) && n >= 6) {
             result = "Fizz";
         }
         if (isDivisibleBy5(n)) {
             result = "Buzz";
         }
-        if (isDivisibleBy5(n) && isDivisibleBy(n, 3) && n >= 30) {
+        if (isDivisibleBy5(n) && isDivisibleBy3(n) && n >= 30) {
             result = "FizzBuzz";
         }
         if (result == null) {
             result = String.valueOf(n);
         }
         return result;
+    }
+
+    private boolean isDivisibleBy3(int n) {
+        return isDivisibleBy(n, 3);
     }
 
     private boolean isDivisibleBy5(int n) {
