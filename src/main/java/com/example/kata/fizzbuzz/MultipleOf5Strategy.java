156a17548fad9ed8d36910632133214cdadb39c1
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:59:50 +0200

    red: multiple of 5 should ask the multiple collaborator to calculate the remainder (modulo)

--- src/main/java/com/example/kata/fizzbuzz/MultipleOf5Strategy.java
+++ src/main/java/com/example/kata/fizzbuzz/MultipleOf5Strategy.java
@@ -3,6 +3,12 @@ package com.example.kata.fizzbuzz;
 public class MultipleOf5Strategy implements FizzBuzzStrategy {
 
     public static final String BUZZ = "Buzz";
+    private final MultipleOf multipleCollaborator;
+
+    public MultipleOf5Strategy(MultipleOf multipleCollaborator) {
+
+        this.multipleCollaborator = multipleCollaborator;
+    }
 
     @Override
     public String apply(int n, String existingResult) {
---
+++ src/test/java/com/example/kata/fizzbuzz/MultipleOf5StrategyTest.java
@@ -0,0 +1,21 @@
+package com.example.kata.fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class MultipleOf5StrategyTest {
+
+    @Test
+    public void should_ask_the_multiplication_collaborator(){
+        final MultipleOf multipleCollaborator = mock(MultipleOf.class);
+        FizzBuzzStrategy sut = new MultipleOf5Strategy(multipleCollaborator);
+
+        sut.apply(1,"");
+
+        verify(multipleCollaborator).isMultipleOf(1);
+    }
+
+}
