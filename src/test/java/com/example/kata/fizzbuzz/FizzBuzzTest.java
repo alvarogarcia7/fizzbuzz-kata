c3489b9186b2d7091280af15c01f7dd86913674c
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:21:09 +0200

    refactor: reformat code

--- src/test/java/com/example/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/example/kata/fizzbuzz/FizzBuzzTest.java
@@ -28,16 +28,16 @@ public class FizzBuzzTest {
 
     private String fizzbuzz(int n) {
         String result = null;
-        if(n%3 == 0 && n >= 6) {
-            result= "Fizz";
+        if (n % 3 == 0 && n >= 6) {
+            result = "Fizz";
         }
-        if(n%5 == 0) {
-            result= "Buzz";
+        if (n % 5 == 0) {
+            result = "Buzz";
         }
         if (n % 5 == 0 && n % 3 == 0 && n >= 30) {
             result = "FizzBuzz";
         }
-        if(result == null){
+        if (result == null) {
             result = String.valueOf(n);
         }
         return result;
