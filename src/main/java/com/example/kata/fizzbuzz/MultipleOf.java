ba5806b94627e5b50ab666aef1c1b60aa2580039
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:57:19 +0200

    add TODO for later solving it

--- src/main/java/com/example/kata/fizzbuzz/MultipleOf.java
+++ src/main/java/com/example/kata/fizzbuzz/MultipleOf.java
@@ -2,6 +2,7 @@ package com.example.kata.fizzbuzz;
 
 public class MultipleOf {
     public boolean isMultipleOf(int n) {
+        //TODO AGB how it is possible that this works?
         return true;
     }
 }
