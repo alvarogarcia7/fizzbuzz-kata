a21657ede6841dfb9e04dc64c67eaa540b153235
Alvaro Garcia <derecerca@gmail.com>
2015-05-28 07:26:52 +0200

    refactor: extract method for checking divisibility by 5, using the previously extracted method

--- src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
@@ -6,16 +6,20 @@ public class FizzBuzz {
         if (isDivisibleBy(n, 3) && n >= 6) {
             result = "Fizz";
         }
-        if (n % 5 == 0) {
+        if (isDivisibleBy5(n)) {
             result = "Buzz";
         }
-        if (n % 5 == 0 && isDivisibleBy(n, 3) && n >= 30) {
+        if (isDivisibleBy5(n) && isDivisibleBy(n, 3) && n >= 30) {
             result = "FizzBuzz";
         }
         if (result == null) {
             result = String.valueOf(n);
         }
         return result;
+    }
+
+    private boolean isDivisibleBy5(int n) {
+        return isDivisibleBy(n, 5);
     }
 
     private boolean isDivisibleBy(int n, int divisor) {
