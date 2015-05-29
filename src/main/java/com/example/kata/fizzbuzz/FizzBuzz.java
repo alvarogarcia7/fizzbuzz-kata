0d5bb2941bd827062bdcca1c6f74eaeb32e81944
alvaro garcia <derecerca@gmail.com>
2015-05-29 17:38:20 +0200

    refactor: reformat code

--- src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/kata/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,15 @@ package com.example.kata.fizzbuzz;
 
 public class FizzBuzz {
 
-    private final FizzBuzzStrategy[] strategies;
+	private final FizzBuzzStrategy[] strategies;
 
-    public FizzBuzz (FizzBuzzStrategy... strategies){
-        this.strategies = strategies;
-    }
+	public FizzBuzz (FizzBuzzStrategy... strategies) {
+		this.strategies = strategies;
+	}
 
-    void getFor (int n) {
-        for(FizzBuzzStrategy current : strategies) {
-            current.apply(n);
-        }
-    }
+	void getFor (int n) {
+		for (FizzBuzzStrategy current : strategies) {
+			current.apply(n);
+		}
+	}
 }
