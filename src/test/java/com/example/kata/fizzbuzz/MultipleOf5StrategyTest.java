0a875118380d1d22497e278eb4771cc5e81ec2e5
alvaro garcia <derecerca@gmail.com>
2015-05-29 16:15:48 +0200

    add test for verifying the connection between multiple and its collaborator

--- src/test/java/com/example/kata/fizzbuzz/MultipleOf5StrategyTest.java
+++ src/test/java/com/example/kata/fizzbuzz/MultipleOf5StrategyTest.java
@@ -1,11 +1,17 @@
 package com.example.kata.fizzbuzz;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class MultipleOf5StrategyTest {
+
+    private static final int ANY_VALUE = 0;
 
     @Test
     public void should_ask_the_multiplication_collaborator(){
@@ -15,6 +21,20 @@ public class MultipleOf5StrategyTest {
         sut.apply(1,"");
 
         verify(multipleCollaborator).isMultipleOf(1);
+    }
+
+    @Test
+    public void should_add_fizz_when_the_condition_holds () {
+        final MultipleOf multipleCollaborator = stub(MultipleOf.class);
+        doReturn(true).when(multipleCollaborator).isMultipleOf(anyInt());
+
+        final MultipleOf5Strategy sut = new MultipleOf5Strategy(multipleCollaborator);
+
+        MatcherAssert.assertThat(sut.apply(ANY_VALUE, ""), is("Buzz"));
+    }
+
+    private <T> T stub (final Class<T> clazz) {
+        return mock(clazz);
     }
 
 }
