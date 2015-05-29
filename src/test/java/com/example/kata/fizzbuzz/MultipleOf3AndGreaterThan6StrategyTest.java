8512fed53a971e035a03224a5ecfc8ac396de128
alvaro garcia <derecerca@gmail.com>
2015-05-29 16:13:16 +0200

    add test for verifying the connection between multiple and its collaborator

--- src/test/java/com/example/kata/fizzbuzz/MultipleOf3AndGreaterThan6StrategyTest.java
+++ src/test/java/com/example/kata/fizzbuzz/MultipleOf3AndGreaterThan6StrategyTest.java
@@ -1,8 +1,11 @@
 package com.example.kata.fizzbuzz;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -16,6 +19,20 @@ public class MultipleOf3AndGreaterThan6StrategyTest {
         sut.apply(1,"");
 
         verify(multipleCollaborator).isMultipleOf(1);
+    }
+
+    @Test
+    public void should_add_fizz_when_the_condition_holds () {
+        final MultipleOf multipleCollaborator = stub(MultipleOf.class);
+        doReturn(true).when(multipleCollaborator).isMultipleOf(anyInt());
+
+        final MultipleOf3AndGreaterThan6Strategy sut = new MultipleOf3AndGreaterThan6Strategy(multipleCollaborator);
+
+        MatcherAssert.assertThat(sut.apply(6,""), is("Fizz"));
+    }
+
+    private <T> T stub (final Class<T> classToMock) {
+        return mock(classToMock);
     }
 
 }
