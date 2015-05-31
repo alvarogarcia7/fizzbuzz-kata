0c7316d38650e89889bc92d71e6acad42751a99f
alvaro garcia <derecerca@gmail.com>
2015-05-31 10:30:21 +0300

    refactor: remove duplication, extracting the sut creation to the setup

--- src/test/java/com/example/kata/fizzbuzz/FizzBuzzResultTest.java
+++ src/test/java/com/example/kata/fizzbuzz/FizzBuzzResultTest.java
@@ -1,5 +1,6 @@
 package com.example.kata.fizzbuzz;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,15 +8,21 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzResultTest {
 
+	private FizzBuzzResult fizzBuzzResult;
+
+	@Before
+	public void setUp () throws Exception {
+		fizzBuzzResult = new FizzBuzzResult();
+	}
+
 	@Test
 	public void should_be_empty_by_default () {
 
-		assertThat(new FizzBuzzResult().isEmpty(), is(true));
+		assertThat(fizzBuzzResult.isEmpty(), is(true));
 	}
 
 	@Test
 	public void should_not_be_empty_after_adding_a_suffix () {
-		final FizzBuzzResult fizzBuzzResult = new FizzBuzzResult();
 		fizzBuzzResult.append("any");
 		assertThat(fizzBuzzResult.isEmpty(), is(false));
 	}
