ec53841b27372bdf018f015572f55285784b4b7f
alvaro garcia <derecerca@gmail.com>
2015-05-31 10:55:15 +0300

    remove all WIP (it still is on another branch)

--- src/main/java/com/example/kata/fizzbuzz/ConditionalExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.example.kata.fizzbuzz;
-
-import com.example.kata.fizzbuzz.strategy.MultipleOf;
-
-public interface ConditionalExecutor {
-	public void applyIfMatches(MultipleOf multipleOf, Runnable runnable);
-}
--- src/main/java/com/example/kata/fizzbuzz/Executor.java
+++
@@ -1,7 +0,0 @@
-package com.example.kata.fizzbuzz;
-
-public class Executor {
-	public void execute (final int n, final Condition condition, final Action action) {
-		condition.executeIfApplies(n, action);
-	}
-}
